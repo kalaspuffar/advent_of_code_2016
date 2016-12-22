506299d2548fdef3bad2bb0f82c86ee04796dca6
Daniel Persson <mailto.woden@gmail.com>
2016-12-22 15:55:07 +0100

    One down 49 to go.

---
+++ kalaspuffar-ruby/dec1_1.rb
@@ -0,0 +1,30 @@
+direction, x, y = 0, 0, 0
+instructions = "R2, L5, L4, L5, R4, R1, L4, R5, R3, R1, L1, L1, R4, L4, L1, R4, L4, R4, L3, R5, R4, R1, R3, L1, L1, R1, L2, R5, L4, L3, R1, L2, L2, R192, L3, R5, R48, R5, L2, R76, R4, R2, R1, L1, L5, L1, R185, L5, L1, R5, L4, R1, R3, L4, L3, R1, L5, R4, L4, R4, R5, L3, L1, L2, L4, L3, L4, R2, R2, L3, L5, R2, R5, L1, R1, L3, L5, L3, R4, L4, R3, L1, R5, L3, R2, R4, R2, L1, R3, L1, L3, L5, R4, R5, R2, R2, L5, L3, L1, L1, L5, L2, L3, R3, R3, L3, L4, L5, R2, L1, R1, R3, R4, L2, R1, L1, R3, R3, L4, L2, R5, R5, L1, R4, L5, L5, R1, L5, R4, R2, L1, L4, R1, L1, L1, L5, R3, R4, L2, R1, R2, R1, R1, R3, L5, R1, R4"
+instructions.split(", ").each {
+  |s|
+
+  if s[0] == "R"
+    direction += 1
+  else
+    direction -= 1
+  end
+
+  if(direction > 3)
+    direction = 0
+  end
+  if(direction < 0)
+    direction = 3
+  end
+
+  case direction
+    when 0
+      x += s.byteslice(1..5).to_i
+    when 1
+      y += s.byteslice(1..5).to_i
+    when 2
+      x -= s.byteslice(1..5).to_i
+    when 3
+      y -= s.byteslice(1..5).to_i
+  end
+}
+puts x.abs + y.abs
